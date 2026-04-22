fa6b1235fc29e7c1962b6fb548c7485242156e95
derundevu <derundevu@gmail.com>
2026-04-22 03:02:17 +0300

    bump version to 1.2.1, changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = calcVersionCode()
-        versionName = "1.2.0"
+        versionName = "1.2.1"
     }
 
     buildFeatures {
